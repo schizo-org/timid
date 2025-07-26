0759944d03209a01ec3bae23505349e119d28425
Bloxx12 <charlie@charlieroot.dev>
2025-07-26 23:44:32 +0200

    modules: default: fix extensions description
    Co-authored-by: NotAShelf <raf@notashelf.dev>
    Signed-off-by: Bloxx12 <charlie@charlieroot.dev>
    Change-Id: I6a6a69649c958ffc5384d64f6e986e172096d140

--- modules/default.nix
+++ modules/default.nix
@@ -29,32 +29,40 @@ in
         extensions = mkOption {
           type = listOf string;
           default = [ ];
-          description = ''
-            List of extensions to be installed.
-
-            You can add new extensions by their chrome web-store ID.
-            For example, the url of Ublock Origin Lite is:
-            https://chromewebstore.google.com/detail/ublock-origin-lite/ddkjiahejlhfcafbddmgiahcphecmpfh
-            Take the last part, `ddkjiahejlhfcafbddmgiahcphecmpfh` and
-            you have the ID of the extension.
-          '';
           example = literalExpression ''
-            # NoScript
-            "doojmbjmlfjjnbmnoijecmcbfeoakpjm"
-            # KeePassXC-Browser
-            "oboonakemofpalcgghocfoadofidjkkk"
-            # Catppuccin Mocha
-            "bkkmolkhemgaeaeggcmfbghljjjoofoh"
-            # Dark Reader
-            "eimadpbcbfnmbkopoojfekhnkhdbieeh"
-            # UBlock Origin
-            "cjpalhdlnbpafiamejdnhcphjbkeiagm"
-            # I still don't care about cookies
-            "edibdbjcniadpccecjdfdjjppcpchdlm"
-            # Sponsorblock
-            "mnjggcdmjocbbbhaepdhchncahnbgone"
-            # Decentraleyes
-            "ldpochfccmkkmhdbclfhpagapcfdljkj"
+            [
+              # NoScript
+              "doojmbjmlfjjnbmnoijecmcbfeoakpjm"
+              # KeePassXC-Browser
+              "oboonakemofpalcgghocfoadofidjkkk"
+              # Catppuccin Mocha
+              "bkkmolkhemgaeaeggcmfbghljjjoofoh"
+              # Dark Reader
+              "eimadpbcbfnmbkopoojfekhnkhdbieeh"
+              # UBlock Origin
+              "cjpalhdlnbpafiamejdnhcphjbkeiagm"
+              # I still don't care about cookies
+              "edibdbjcniadpccecjdfdjjppcpchdlm"
+              # Sponsorblock
+              "mnjggcdmjocbbbhaepdhchncahnbgone"
+              # Decentraleyes
+              "ldpochfccmkkmhdbclfhpagapcfdljkj"
+            ]
+          '';
+          description = ''
+            A list of of extension IDs to be installed by default.
+
+            ::: {.note}
+
+            You can add new extensions by their Chrome web-store ID. For example
+            the URL of UBlock Origin Lite is as follows:
+
+            `https://chromewebstore.google.com/detail/ublock-origin-lite/ddkjiahejlhfcafbddmgiahcphecmpfh`
+
+            and if you take the last part, `ddkjiahejlhfcafbddmgiahcphecmpfh` you
+            will have the extension ID.
+
+            :::
           '';
         };
       };
